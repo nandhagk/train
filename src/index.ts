c0a1ab4f2543d4bfc0bb62f881ab669f1874961f
nandhagk <nandhagopi@gmail.com>
2024-06-11 19:16:17 +0530

    feat: add edgedb

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('Hello World!');
+import e from '#db';
+import * as edgedb from 'edgedb';
+
+const client = edgedb.createClient();
+
+async function main() {
+	// result will be inferred based on the query
+	const result = await e
+		.select(e.Movie, () => ({
+			title: true,
+			actors: () => ({ name: true }),
+			filter_single: { title: 'Iron Man 2' }
+		}))
+		.run(client);
+
+	console.log(JSON.stringify(result, null, 2));
+}
+
+main();
