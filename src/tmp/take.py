cff0c40807d1f283e7cbef3e4a5120a8ecf7fcfe
Dragon-KK <61528852+Dragon-KK@users.noreply.github.com>
2024-06-13 16:00:16 +0530

    fixed data

--- src/tmp/take.py
+++ src/tmp/take.py
@@ -2,3 +2,19 @@ def handler(raw: str):
     
     if raw == "MLPM": return [raw, raw]
 
+
+def fix():
+    import json
+    from pathlib import Path
+    dp = Path(__file__).with_name("mas.json")
+    data = json.loads(dp.read_text())
+
+    for block in data:
+        for i in range(len(data[block])):
+            if type(data[block][i]) == str:
+                data[block][i] = [data[block][i], data[block][i]]
+    dp.write_text(json.dumps(data))
+
+if __name__ == '__main__':
+    # fix()
+    pass
