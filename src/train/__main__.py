05a5b4da97ce60a3d91519df8c819955520d181e
Dragon-KK <61528852+Dragon-KK@users.noreply.github.com>
2024-06-13 23:16:01 +0530

    data exists now

--- src/train/__main__.py
+++ src/train/__main__.py
@@ -13,7 +13,8 @@ cur.execute(
     CREATE TABLE IF NOT EXISTS block (
         id INTEGER PRIMARY KEY,
         line VARCHAR(25) NOT NULL,
-        name VARCHAR(25) UNIQUE NOT NULL
+        name VARCHAR(25) NOT NULL,
+        UNIQUE(line, name)
     )
     """,
 )
@@ -35,10 +36,11 @@ cur.execute(
     """
     CREATE TABLE IF NOT EXISTS station (
         id INTEGER PRIMARY KEY,
-        name VARCHAR(25) UNIQUE NOT NULL,
+        name VARCHAR(25) NOT NULL,
 
         block_id INTEGER NOT NULL,
-        FOREIGN KEY(block_id) REFERENCES block(id)
+        FOREIGN KEY(block_id) REFERENCES block(id),
+        UNIQUE(name, block_id)
     );
     """,
 )
@@ -83,7 +85,119 @@ def main() -> None:
 @main.command()
 @click.argument("data", type=click.Path(exists=True, dir_okay=False, resolve_path=True))
 def init(data: str):
-    print(data)
+    """
+    Puts some dummy data in the database.
+    NOTE: Does not populate free time table.
+    """
+    def init_block(block_name: str):
+        block_ids = [0, 0]
+        for line in ("UP", "DN"):
+            cur.execute(
+                """
+                INSERT INTO block (id, line, name)
+                SELECT NULL, ?, ? WHERE
+                NOT EXISTS (SELECT * from block where name = ? and line = ?)
+                """,
+                (line, block_name, block_name, line)
+            )
+            con.commit()
+
+            cur.execute("select id from block where name = ?", (block_name,))
+            block_ids[line == "DN"] = cur.fetchall()[0][0]
+
+        return block_ids
+
+    def init_station(station_name: str, block_id: int):
+        try:
+            cur.execute(
+                """
+                INSERT INTO station (id, name, block_id)
+                SELECT NULL, ?, ? WHERE
+                NOT EXISTS (SELECT * from station where name = ? and block_id = ?)
+                """,
+                (station_name, block_id, station_name, block_id)
+            )
+            con.commit()
+
+            cur.execute("select id from station where name = ? and block_id = ?", (station_name, block_id))
+            return cur.fetchall()[0][0]
+        except:
+            return None
+        
+    def init_section(id1: int, id2: int):
+        cur.execute(
+            """
+            INSERT INTO section (id, from_id, to_id)
+            SELECT NULL, ?, ? WHERE
+            NOT EXISTS (SELECT * from section where from_id = ? and to_id = ?)
+            """,
+            (id1, id2, id1, id2)
+        )
+        con.commit()
+
+    import json
+    from pathlib import Path
+    blocks = json.loads(Path(data).read_text())
+    for block in blocks:
+        for block_id in init_block(block):
+            for section in blocks[block]:
+                if section[0] == section[1]:
+                    section[0] = section[1] = section[0] + "_YD"
+                s1 = init_station(section[0], block_id)
+                s2 = init_station(section[1], block_id)
+
+                if s1 == None or s2 == None:
+                    print(f"Skipping `{block}`: {tuple(section)}")
+                    continue
+
+                init_section(s1, s2)
+
+@main.command()
+@click.argument("data", type=click.Path(exists=True, dir_okay=False, resolve_path=True))
+@click.argument("length", type=int)
+@click.option("--clear", is_flag = True, default = False)
+def sft(data: str, length: int, clear: bool):
+    """
+    Populateds the free_time table.
+    """
+    import sys
+
+    def get_block_id(block_name: str, ln: str):
+        cur.execute("SELECT id FROM block WHERE name=? and line=?", (block_name, ln))
+        result = cur.fetchone()
+        if result is None:
+            print(block_name)
+            print("ERROR! Block does not exist", file=sys.stderr)
+            sys.exit(1)
+
+        return result[0]
+
+    if clear:
+        cur.execute("DELETE FROM free_time")
+        con.commit()
+
+    from pathlib import Path
+    from datetime import date
+    raw = Path(data)
+    for block_data in raw.read_text().split('\n\n'):
+        lines = block_data.splitlines()
+        block_name = lines[0][1:]
+        for line in lines[1:]:
+            ln, st, et = line.split()
+            st = timedelta(hours=int(st[:2]), minutes=int(st[2:]))
+            et = timedelta(hours=int(et[:2]), minutes=int(et[2:]))
+            bid = get_block_id(block_name, ln)
+            cur.executemany("INSERT INTO free_time VALUES (NULL, ?, ?, ?)",
+                [
+                    (
+                        bid,
+                        datetime.strftime(datetime.now().replace(hour = 0, second = 0, minute = 0) + timedelta(days=days) + st, "%Y-%m-%d %H:%M:%S"),
+                        datetime.strftime(datetime.now().replace(hour = 0, second = 0, minute = 0) + timedelta(days=days) + et, "%Y-%m-%d %H:%M:%S")
+                    )
+                    for days in range(length)
+            ])
+            con.commit()
+
 
 
 @main.command()
