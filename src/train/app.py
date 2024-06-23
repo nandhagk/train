cf13b6f603a8273b2e4e04a52e31e8ce56d3d1d4
nandhagk <nandhagopi@gmail.com>
2024-06-23 17:01:21 +0530

    use tempfile

--- src/train/app.py
+++ src/train/app.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from collections import defaultdict
 from logging import getLogger
 from pathlib import Path
+from tempfile import NamedTemporaryFile
 
 from flask import Flask, request, send_file
 
@@ -19,54 +20,54 @@ def idk():
     return send_file((Path.cwd() / "client" / "index.html").as_posix())
 
 
-count = 0
-
 logger = getLogger(__name__)
 
 
 @app.post("/request")
 def handle_form():
     """Handle form."""
-    global count
-
     f = request.files["fileInput"]
     assert f.filename is not None
 
     name, _, ext = f.filename.rpartition(".")
 
-    src = Path.cwd() / "tmp" / (str(count := count + 1) + "." + ext)
-    f.save(src.as_posix())
+    with (
+        NamedTemporaryFile(suffix=f".{ext}") as src,
+        NamedTemporaryFile(suffix=f".{ext}") as dst,
+    ):
+        src_path = Path(src.name)
+        dst_path = Path(dst.name)
 
-    con = get_db()
-    cur = con.cursor()
+        f.save(src_path)
 
-    dst = src.parent / (str(count := count + 1) + "." + ext)
+        con = get_db()
+        cur = con.cursor()
 
-    try:
-        taskqs_per_section: dict[int, list[PartialTask]] = defaultdict(list)
+        try:
+            taskqs_per_section: dict[int, list[PartialTask]] = defaultdict(list)
 
-        fm = FileManager.get_manager(src)(src, dst)
-        for taskq in fm.read(cur):
-            taskqs_per_section[taskq.section_id].append(taskq)
+            fm = FileManager.get_manager(src_path)(src_path, dst_path)
+            for taskq in fm.read(cur):
+                taskqs_per_section[taskq.section_id].append(taskq)
 
-        tasks = []
-        for section_id, taskqs in taskqs_per_section.items():
-            logger.info("Scheduling section: %d", section_id)
-            try:
-                tasks.extend(Task.insert_many(cur, taskqs))
-            except Exception:
-                logger.exception("Ignoring section: %d", section_id)
+            tasks = []
+            for section_id, taskqs in taskqs_per_section.items():
+                logger.info("Scheduling section: %d", section_id)
+                try:
+                    tasks.extend(Task.insert_many(cur, taskqs))
+                except Exception:
+                    logger.exception("Ignoring section: %d", section_id)
 
-        con.commit()
-        fm.write(cur, tasks)
+            con.commit()
+            fm.write(cur, tasks)
 
-        logger.info("Populated database and saved output file: %s", dst)
-    except Exception as e:
-        logger.exception("Failed to populate database from file")
-        return repr(e)
+            logger.info("Populated database and saved output file: %s", dst)
+        except Exception as e:
+            logger.exception("Failed to populate database from file")
+            return repr(e)
 
-    return send_file(
-        dst.as_posix(),
-        as_attachment=True,
-        download_name=f"{name}_output.{ext}",
-    )
+        return send_file(
+            dst_path.as_posix(),
+            as_attachment=True,
+            download_name=f"{name}_output.{ext}",
+        )
