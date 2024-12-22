cb9f5e50e2cedbdcfad4d2a206473187226df3fc
nandhagk <nandhagopi@gmail.com>
2024-12-22 09:52:53 +0530

    fix title and icon

--- src/client/src/components/version-switcher.tsx
+++ src/client/src/components/version-switcher.tsx
@@ -23,7 +23,7 @@ export function VersionSwitcher() {
                 <GalleryVerticalEnd className="size-4" />
               </div>
               <div className="flex flex-col gap-0.5 leading-none">
-                <span className="font-semibold">Train</span>
+                <span className="font-semibold">FTCB</span>
               </div>
             </SidebarMenuButton>
 
